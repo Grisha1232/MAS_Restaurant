d96c3e4e82f3101e690ef79981cea17d12d59659
rococo4 <aalebedev_8@edu.hse.ru>
2023-03-22 21:10:34 +0300

    setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
 
+import jade.core.*;
+import jade.core.Runtime;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+import org.example.agents.ProductAgent;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws StaleProxyException {
+        final Runtime rt = Runtime.instance();
+        final Profile p = new ProfileImpl();
+
+        p.setParameter(Profile.MAIN_HOST, "localhost");
+        p.setParameter(Profile.MAIN_PORT, "8080");
+        p.setParameter(Profile.GUI, "true");
+        var r = rt.createMainContainer(p);
+        r.createNewAgent("ag", ProductAgent.class.getName(),null ).start();
+
     }
 }
--- src/main/java/org/example/agents/ProductAgent.java
+++ src/main/java/org/example/agents/ProductAgent.java
@@ -1,6 +1,56 @@
 package org.example.agents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Optional;
 
 public class ProductAgent extends Agent {
+    int prodTypeId;
+    String prodTypeName;
+    boolean prodIsFood;
+    boolean isReserved;
+    AID storage;
+
+
+    @Override
+    protected void setup() {
+        Object[] args = getArguments();
+        prodTypeId = (int) args[0];
+        prodTypeName = (String) args[1];
+        prodIsFood = (boolean) args[2];
+        storage = (AID) args[3];
+        isReserved = true;
+
+        addBehaviour(new OneShotBehaviour() {
+            @Override
+            public void action() {
+                myAgent.addBehaviour(new Reserve());
+            }
+        });
+    }
+
+
+    private static class Reserve extends Behaviour {
+
+        @Override
+        public void action() {
+            var msg = myAgent.receive();
+            if (msg != null) {
+
+            } else {
+                block();
+            }
+        }
+
+        @Override
+        public boolean done() {
+            return true;
+        }
+    }
 }
