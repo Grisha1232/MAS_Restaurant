dcc2e590ad5f5b8be6b639f21c8ac655afa640c2
Grisha1232 <grishaseleznev@yandex.ru>
2023-03-23 16:52:01 +0300

    add structure to equipmentAgent

--- src/main/java/org/example/agents/EquipmentAgent.java
+++ src/main/java/org/example/agents/EquipmentAgent.java
@@ -1,6 +1,29 @@
 package org.example.agents;
 
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 
 public class EquipmentAgent extends Agent {
+    Integer equipmentType;
+    String equipmentName;
+    Boolean isEquipmentActive = false;
+    @Override
+    protected void setup() {
+        // TODO: type, name and active from args
+        // getArguments()
+    }
+
+    private class ReserveForOperation extends Behaviour {
+
+        @Override
+        public void action() {
+            isEquipmentActive = !isEquipmentActive;
+        }
+
+        @Override
+        public boolean done() {
+            return true;
+        }
+    }
+
 }
