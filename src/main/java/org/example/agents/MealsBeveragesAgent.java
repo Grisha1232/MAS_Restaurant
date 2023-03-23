f5b1e87d7241a04cdb18ad16b5a52fea4e803a2a
Grisha1232 <grishaseleznev@yandex.ru>
2023-03-23 17:05:45 +0300

    add cooking behaviour to mealsBeveragesAgent

--- src/main/java/org/example/agents/MealsBeveragesAgent.java
+++ src/main/java/org/example/agents/MealsBeveragesAgent.java
@@ -3,28 +3,40 @@ package org.example.agents;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
 
-import java.util.Date;
 import java.util.Deque;
 
 public class MealsBeveragesAgent extends Agent {
-
+    String mealName;
+    Integer ordDish;
+    Integer processorID;
+    Deque<AID> operations;
 
     @Override
     protected void setup() {
         // TODO: get from arguments what need to cook
+        addBehaviour(new CookMeal());
     }
 
     private class CookMeal extends Behaviour {
-
+        boolean isDone = false;
         @Override
         public void action() {
-
+            if (!operations.isEmpty()) {
+                var operation = operations.getFirst();
+                var msg = new ACLMessage(ACLMessage.INFORM);
+                msg.addReceiver(operation);
+                msg.setContent("start");
+                send(msg);
+            } else {
+                isDone = true;
+            }
         }
 
         @Override
         public boolean done() {
-            return false;
+            return isDone;
         }
     }
 }
