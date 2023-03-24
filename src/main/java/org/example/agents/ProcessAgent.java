01436f6955f28dadb0f25820fff1cbf9923f38cd
rococo4 <aalebedev_8@edu.hse.ru>
2023-03-24 23:24:49 +0300

    Add Cooks to models, add ProcessAgent

--- src/main/java/org/example/agents/ProcessAgent.java
+++ src/main/java/org/example/agents/ProcessAgent.java
@@ -1,6 +1,84 @@
 package org.example.agents;
 
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+import org.example.models.Cooks;
+import org.example.models.KitchenEquipment;
+
+import java.util.ArrayList;
 
 public class ProcessAgent extends Agent {
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+
+    boolean allIsAvailable = true;
+
+    private class ReserveEquipment extends Behaviour {
+        @Override
+        public void action() {
+            // TODO: при многоповторке не особо понятно как будет работать ниже строчка.
+            ACLMessage msg = myAgent.receive();
+            if (msg != null) {
+                try {
+                   var machinesToReserve =(ArrayList<KitchenEquipment>)msg.getContentObject();
+                    for (KitchenEquipment kitchenEquipment : machinesToReserve) {
+                        if (!kitchenEquipment.equip_active) {
+                            allIsAvailable = false;
+                            break;
+                        }
+                    }
+                    if (allIsAvailable) {
+                        for (var i : machinesToReserve) {
+                            i.equip_active = false;
+                        }
+                    }
+                } catch (UnreadableException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                block();
+            }
+        }
+
+        @Override
+        public boolean done() {
+            return allIsAvailable;
+        }
+    }
+    private class ReserveCooks extends Behaviour {
+        @Override
+        public void action() {
+            // TODO: при многоповторке не особо понятно как будет работать ниже строчка.
+            ACLMessage msg = myAgent.receive();
+            if (msg != null) {
+                try {
+                    var cooksToReserve =(ArrayList<Cooks>)msg.getContentObject();
+                    for (Cooks i : cooksToReserve) {
+                        if (!i.cook_active) {
+                            allIsAvailable = false;
+                            break;
+                        }
+                    }
+                    if (allIsAvailable) {
+                        for (var i : cooksToReserve) {
+                            i.cook_active = false;
+                        }
+                    }
+                } catch (UnreadableException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                block();
+            }
+        }
+
+        @Override
+        public boolean done() {
+            return allIsAvailable;
+        }
+    }
 }
--- src/main/java/org/example/models/KitchenEquipment.java
+++ src/main/java/org/example/models/KitchenEquipment.java
@@ -3,9 +3,10 @@ package org.example.models;
 import java.io.Serializable;
 
 public class KitchenEquipment implements Serializable {
-    int equip_type;
-    String equip_name;
-    boolean equip_active;
+    public int equip_type;
+    public String equip_name;
+    public boolean equip_active;
+
     KitchenEquipment(int type, String name, boolean active) {
         equip_type = type;
         equip_name = name;
