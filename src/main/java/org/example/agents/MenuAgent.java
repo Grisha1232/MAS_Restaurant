9a622125f3a0a17c2ba7f9c3e907692f48a83eea
Grisha1232 <grishaseleznev@yandex.ru>
2023-03-26 11:27:25 +0300

    changed behaviour of menu agent

--- src/main/java/org/example/agents/MenuAgent.java
+++ src/main/java/org/example/agents/MenuAgent.java
@@ -6,6 +6,7 @@ import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.UnreadableException;
 import org.example.Pair;
+import org.example.models.Visitor.VisOrdDishes;
 import org.example.models.Visitor.Visitor;
 
 import java.io.IOException;
@@ -15,53 +16,48 @@ public class MenuAgent extends Agent {
 
     @Override
     protected void setup() {
-
-        addBehaviour(new ActualizeMenu());
-        addBehaviour(new SendAboutMenu());
+        addBehaviour(new ProcessTheReceivedMessages());
     }
 
-    private class ActualizeMenu extends CyclicBehaviour {
-        @Override
-        public void action() {
-            System.out.println("Menu: trying to receive message from manager");
-            var msg = myAgent.receive();
-            if (msg != null && !msg.getSender().getLocalName().equals("Storage")) {
-                try {
-                    Visitor vis = (Visitor) msg.getContentObject();
-                    var message = new ACLMessage(ACLMessage.INFORM);
-                    message.addReceiver(new AID("Storage", AID.ISLOCALNAME));
-                    message.setContentObject(vis);
-                    send(message);
-                } catch (UnreadableException | IOException e) {
-                    throw new RuntimeException(e);
-                }
 
-            } else {
-                block();
-            }
-        }
-    }
-    //TODO: дописать чтоб из блюда получалась операция.
-    private class SendAboutMenu extends CyclicBehaviour {
+    private class ProcessTheReceivedMessages extends CyclicBehaviour {
         @Override
         public void action() {
-            System.out.println("Menu: trying to receive message from Storage");
-            var msg = myAgent.receive();
-            if (msg != null && msg.getSender().getLocalName().equals("Storage")) {
-                try {
-                    var pair = (Pair<Visitor, ArrayList<Integer>>)msg.getContentObject();
-                    var vis = pair.getFirst();
-                    var unv = pair.getSecond();
-                    // Удаляем блюда, которые не можем приготовить
-                    for (var i : unv) {
-                        vis.vis_ord_dishes.removeIf((x) -> x.menu_dish == i);
+            var msg = receive();
+            if (msg != null) {
+                if (msg.getSender().getLocalName().equals("Storage")) {
+                    try {
+                        var pair = (Pair<Visitor, ArrayList<Integer>>)msg.getContentObject();
+                        var vis = pair.getFirst();
+                        var unv = pair.getSecond();
+                        // Удаляем блюда, которые не можем приготовить
+                        for (var i : unv) {
+                            vis.vis_ord_dishes.removeIf((x) -> x.menu_dish == i);
+                        }
+                        var message = new ACLMessage(ACLMessage.INFORM);
+                        message.addReceiver(new AID("Manager", AID.ISLOCALNAME));
+                        message.setContentObject(vis);
+                        send(message);
+                    } catch (UnreadableException | IOException e) {
+                        throw new RuntimeException(e);
                     }
-                    var message = new ACLMessage(ACLMessage.INFORM);
-                    message.addReceiver(new AID("Manager", AID.ISLOCALNAME));
-                    message.setContentObject(vis);
-                    send(message);
-                } catch (UnreadableException | IOException e) {
-                    throw new RuntimeException(e);
+                } else if (msg.getSender().getLocalName().equals("Manager")) {
+                    try {
+                        Visitor vis = (Visitor) msg.getContentObject();
+                        var message = new ACLMessage(ACLMessage.INFORM);
+                        message.addReceiver(new AID("Storage", AID.ISLOCALNAME));
+                        message.setContentObject(vis);
+                        send(message);
+                    } catch (UnreadableException | IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    try {
+                        var response = (VisOrdDishes) msg.getContentObject();
+                        // TODO: как получить из этого операции?
+                    } catch (UnreadableException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             } else {
                 block();
