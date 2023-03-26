6c261d02a4c9e29f5c87cb8dd7d591a4431f8ff8
Grisha1232 <grishaseleznev@yandex.ru>
2023-03-26 20:17:53 +0300

    added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,20 @@ package org.example;
 import jade.core.*;
 import jade.core.Runtime;
 import jade.wrapper.ControllerException;
+import org.example.Parsing.ParsingVisitor;
 import org.example.agents.ManagerAgent;
-import org.example.agents.MenuAgent;
 import org.example.agents.StorageAgent;
 import org.example.agents.VisitorAgent;
-import org.example.models.Menu;
 import org.example.models.Visitor.Visitor;
 
+import java.io.File;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
 
 public class Main {
-    public static void main(String[] args) throws ControllerException {
+    public static void main(String[] args) throws ControllerException, ParseException {
         final Runtime rt = Runtime.instance();
         final Profile p = new ProfileImpl();
 
@@ -24,6 +25,10 @@ public class Main {
         p.setParameter(Profile.MAIN_PORT, "8080");
         p.setParameter(Profile.GUI, "true");
 
+        ParsingVisitor.getVisitors(new File("").getAbsolutePath() + "/src/main/java/org/example/input/visitor_order.txt");
+        for (var vis : ParsingVisitor.visitors) {
+            System.out.println(vis.vis_name);
+        }
 
         var r = rt.createMainContainer(p);
         // TODO: считывание входных файлов
